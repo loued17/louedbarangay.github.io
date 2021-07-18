2759773bf960565e7a9213bf7f044d0d2240970b
loued17 <84569947+loued17@users.noreply.github.com>
2021-07-18 08:58:23 +0800

    Update indexi.php

--- indexi.php
+++ indexi.php
@@ -6,7 +6,7 @@
 	<title>Individuals</title>
 	<link rel="stylesheet"  href="style9.css">
 	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
-</head>
+
 
 <header>
 	<h1>Individuals</h1>
@@ -21,15 +21,11 @@
 			<li><a href=" searchoff.php">Search BRGY. Officials</a></li>
 			<li><a href="../index.php">Logout</a></li>
 
-		</ul>
 
-	</nav>
-
-</header>
 <body >
 		<div class="headerP" style="width: 15%;margin-top: 60px;color: white;background: #39ca74;text-align: center;border-radius: 10px 10px 5px 5px;border-bottom: none; border :1px solid #39ca74;padding: 10px;margin-left:-4px   ">
 	<h2>My Information</h2>
-</div>
+
 
 <form method="post" action="index.php"  class="infoP" style="margin-left:-1px; margin-top:0px ;width: 40%;padding: 20px;border :3px solid #39ca74 ;background: white; border-radius: 10px 10px 10px 10px;">
 	
@@ -58,26 +54,12 @@
 	 	   <br>
     
 
-	 	   </div>
     
 	
 
 	 	   	<div class="input-group">
 		<button type="submit" name="appointmenthistory" class="btn" style=" border-radius: 5px;margin-left: 80%; border:none;padding: 10px 20px 10px 20px">My Appointment History</button>
-	
-</div>
+	div>
 	<div class="input-group">
 		<button type="submit" name="feedback" class="btn" style=" border-radius: 5px;margin-left: 80%; border:none;padding: 10px 30px 10px 30px">Send Feedback</button>
-	</div>
-
-  
-</form>
-
 	
-</form>
-
-
-
-</body>
-</html>
-
